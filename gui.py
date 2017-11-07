ee770373097e0688d83687a80a4a0a6411d4e28a
Arman.K <arman.koldaguc@student.hu.nl>
2017-11-07 17:13:21 +0100

    Versie 0.1

--- gui.py
+++ gui.py
@@ -1,40 +1,54 @@
 import tkinter
 
-
 def verander_led_groen():
     global groene_led
     global rode_led
+    global gele_led
     global counter
 
     if counter == 0:
-        groene_led["background"] = "green2"
-        rode_led["background"] = "red4"
-        counter =+ 1
+        groene_led["background"] ="green2"
+        rode_led["background"] ="red4"
+        gele_led["background"] ="yellow4"
+        counter += 1
 
 
 def verander_led_geel():
     global gele_led
+    global rode_led
+    global groene_led
+    global counter
+
     gele_led["background"] = "yellow"
+    rode_led["background"] = "red4"
+    groene_led["background"] = "green4"
+
+    counter +=1
+    root.after(2000, verander_led_rood)
 
 
 def verander_led_rood():
     global rode_led
     global groene_led
+    global gele_led
     global counter
 
     if counter == 1:
-        rode_led["background"] = "red"
+        rode_led["background"] ="red"
         groene_led["background"] ="green4"
-        counter =- 0
+        gele_led["background"] ="yellow4"
+        counter -= 1
 
 
 def toonhoofdmenu():
     hoofdmenuframe.pack()
 
-counter =0
+
 root = tkinter.Tk()
 root.title("Alarm systeem")
 root.configure(background="white")
+
+counter =0
 
 hoofdmenuframe = tkinter.Frame(root)
 hoofdmenuframe.configure(background="white")
@@ -48,6 +62,12 @@ aanknop.grid(row=1, column=0)
 
 uitknop = tkinter.Button(master=hoofdmenuframe, text="UIT", command=verander_led_rood)
 uitknop.grid(row=1, column=1, padx=10)
+
+stopknop = tkinter.Button(master=hoofdmenuframe, text="STOP")
+stopknop.grid(row=1, column=2, padx=10)
+
+alarmknop = tkinter.Button(master=hoofdmenuframe, text="ALARM TESTEN", command=verander_led_geel)
+alarmknop.grid(row=1, column=3)
 
 rode_led = tkinter.Label(master=hoofdmenuframe, background="red4", width=2)
 rode_led.grid(row=2, column=0)
